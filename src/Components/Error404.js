95ef9b7c007069daecd1eb593abfcb3fb155524c
Javier Torres <javiertorres86@outlook.com>
2023-07-19 21:03:36 -0300

    PreEntrega2+Torres

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -2,17 +2,9 @@ import React from "react";
 
 const Error404 = () =>{
     return(
-
-        <div className="container">
-            <div className="row my-5">
-                <div className="col text-center">
-                    <h1>Error <b>404</b></h1>
-                    <h3>La pagina no existe</h3>
-                </div>
-            </div>
-        </div>
-
-
+             <div className="col text-center">
+                <img className="Error" src= ' https://img.freepik.com/vector-premium/estilo-robot-error-404_18591-1536.jpg'/>
+             </div>
     )
 }
 
